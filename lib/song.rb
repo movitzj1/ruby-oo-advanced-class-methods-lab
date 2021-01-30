29001e1c98e73ea9f6bc6273132e5bc6747a565d
Justin Movitz <movitzj1@montclair.edu>
2021-01-30 15:23:10 -0500

    commit tree be16cf068d68e5d8bfc50f10531ab113d00260e4

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,67 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create
+    song.name = song_name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find{|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    self.find_by_name(song_name) || self.create_by_name(song_name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    components = filename.split(" - ")
+    artist_name = components.first
+    #song_name = components[1].slice! ".mp3"
+    song_name = components[1]
+    song_name.slice! ".mp3"
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(filename)
+    components = filename.split(" - ")
+    artist_name = components.first
+    song_name = components[1]
+    components[1].slice! ".mp3"
+    song_name
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
+
+# song = Song.new_from_filename("Thundercat - For Love I Come.mp3")
+# puts song.name
+# puts song.artist_name
